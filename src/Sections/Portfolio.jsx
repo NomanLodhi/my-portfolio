03b3568f82aa778fc857962ec3985acd63e3a875
NomanLodhi <110075584+NomanLodhi@users.noreply.github.com>
2024-03-04 10:59:07 -0800

    Src Folder For All Content

--- src/Sections/Portfolio.jsx
+++ src/Sections/Portfolio.jsx
@@ -13,7 +13,10 @@ export default function Portfolio() {
         <div className=' cursor-pointer h-[200px]  mx-3 my-5 rounded-md relative overflow-hidden hov'  >
     <img src={require(`../${data.imageUrl}`)} alt="" className='aspect-square w-full h-full rounded-md'/>
     <div className="absolute  bg-gradient-to-t to-black/70 from-red-700/70 py-10 w-full h-full rounded-md text-white ani transition-all text-center">
-        <p className="text-white text-center my-10 font-montserrat font-semibold " ><a href={data.url} target='_main' className=' bg-red-500 p-3 rounded-md'>Live Demo  <i class="fa-solid fa-eye px-2"></i></a></p>
+       <div className="flex align-middle justify-center space-x-2">
+       <button className="text-white text-center my-10 font-montserrat font-semibold " ><a href={data.url} target='_main' className=' bg-red-500 p-3 rounded-md'>Live Demo  <i class="fa-solid fa-eye px-2"></i></a></button>
+       <button className="text-white text-center my-10 font-montserrat font-semibold " ><a href={data.githubUrl} target='_main' className=' bg-purple-700 p-3 rounded-md'>View Code  <i class="fa-solid fa-code px-2"></i></a></button>
+       </div>
     </div>
 </div>
     ))}
